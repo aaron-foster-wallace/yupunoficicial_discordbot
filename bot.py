6024b69dcd1ca98493b0387bff7a37799e9b1afd
aaron <aaron.foster.wallace@gmail.com>
2021-04-06 08:45:56 -0400

    clean up

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+import discord
+import os
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
 import requests
 import json
@@ -8,64 +9,69 @@ import time
 import datetime
 from dateutil import tz
 import random
+from markdownify import markdownify
 
-PORT = int(os.environ.get('PORT', 5000))
-
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-import os
- 
-TOKEN =os.environ['TOKEN']
+#from collections import namedtuple 
+#CommandHandler = namedtuple('CommandHandler',['name','age','DOB'])  
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
+
+#### BEGIN TELEGRAM PACH ########
+
+
+class TeleWrapperMessage(object):
+    def __init__(self, message):
+        self.ms=message
+    async def reply_text(self,text):
+        await self.ms.channel.send(text)
+
+    async def reply_html(self,text):
+        await self.ms.channel.send(markdownify(text))
+
+    
+class TeleWrapper(object):
+    def __init__(self, message):        
+        self.message = TeleWrapperMessage(message)
+
+class TeleContext(object):
+    def __init__(self, args):        
+        self.args = args
+
+
+#### END TELEGRAM PACH ########
+async def start(update, context):
     """Send a message when the command /start is issued."""
     html="""       
 Hi! I'm <b>YUP Bot!</b> You can ask me YUP questions like: 
-
-<code>price       - </code>/price
-<code>votelink    - </code>/votelink<code> &lt;voteid&gt;\n\t\t\t  returns the link of given a voteid</code>
-<code>postidof    - </code>/link<code> &lt;url&gt; returns the url id</code>
-<code>votesof     - </code>/votesof<code> &lt;url&gt;\n\t\t\t Return who vote for an adress\n\t\t\t(lista los que votaron por una direccion)</code>
-<code>power       - </code>/power<code> &lt;eos_user&gt;  da la informacion de la carga de la cuenta    </code>    
-<code>top_payment - </code>/top_payment<code> &lt;eos_user&gt; [skip] \n\t\t\t  get the last payment, or the nth last payment</code>
-<code>tp          - </code>/tp<code> &lt;eos_user&gt; [skip]\n\t\t\t  (top_payment shorthand)</code>
-<code>yup         - </code>/yup<code> ?????????</code>
-<code>fee         - </code>/fee<code> bridge fees</code>
-<code>coffe       - </code>/coffe or /cafe <code>\n\t\t\t Buy me a coffe, donation adress</code>
-<code>donar       - </code>/donar<code>\n\t\t\t muestra informacion para hacer una donacion</code>
-<code>help        - </code>/help<code> this text</code>
-
-
-Antiguos/Legacy:
-
-<code>pago        - </code>/pago<code> &lt;voteid&gt;  dice que link pago</code>     
-<code>link        - </code>/link<code> &lt;direcion_web&gt; da el postid de la direccion</code>
-<code>getpayment  - </code>/getpayment<code> &lt;eos_user&gt; [#n]  obtiene el n-último pago</code>
-  
-
+<ul>
+<li><code>price       - </code>$price</li>
+<li><code>votelink    - </code>$votelink<code> &lt;voteid&gt;\n\t\t\t  returns the link of given a voteid</code></li>
+<li><code>postidof    - </code>$link<code> &lt;url&gt; returns the url id</code></li>
+<li><code>votesof     - </code>$votesof<code> &lt;url&gt;\n\t\t\t Return who vote for an adress\n\t\t\t(lista los que votaron por una direccion)</code></li>
+<li><code>power       - </code>$power<code> &lt;eos_user&gt;  da la informacion de la carga de la cuenta    </code>    </li>
+<li><code>top_payment - </code>$top_payment<code> &lt;eos_user&gt; [skip] \n\t\t\t  get the last payment, or the nth last payment</code></li>
+<li><code>tp          - </code>$tp<code> &lt;eos_user&gt; [skip]\n\t\t\t  (top_payment shorthand)</code></li>
+<li><code>yup         - </code>$yup<code> ?????????</code></li>
+<li><code>fee         - </code>$fee<code> bridge fees</code></li>
+<li><code>coffe       - </code>$coffe or $cafe <code>\n\t\t\t Buy me a coffe, donation adress</code></li>
+<li><code>help        - </code>$help<code> this text</code></li>
+</ul>
     """
-    update.message.reply_html(html)
+    await update.message.reply_html(html)
 
-def help(update, context):
+async def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    await start(update, context)
 
-def echo(update, context):
+async def echo(update, context):
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    await update.message.reply_text(update.message.text)
 
-def postidof(update, context):
+async def postidof(update, context):
     """Echo the user message."""
     #link
     url = " ".join(context.args).strip()
-    update.message.reply_text(real_postidof(url))
+    await update.message.reply_text(real_postidof(url))
 
 
 def real_postidof(url):
@@ -86,7 +92,7 @@ def real_postidof(url):
 
 dict_cat={"popularity":"♥️","intelligence":"💡","funny":"😂","easy":"😎","interesting":"⚠️","useful":"🛠️","knowledgeable":"🎓","engaging":"🤔","chill":"❄️","affordable":"💲","beautiful":"🌸","trustworthy": "✅", "wouldelect": "🗳️", "agreewith": "👍", "original": "🦄" }
 
-def votesof(update, context):
+async def votesof(update, context):
     global j
     url = " ".join(context.args).strip()
     postid = real_postidof(url)
@@ -112,12 +118,12 @@ def votesof(update, context):
                 valstr,
                 ""))      
         
-    update.message.reply_html(html)
+    await update.message.reply_html(html)
 
 
 
 
-def votedump(update, context):
+async def votedump(update, context):
  
     """ the user message."""   
     voteid = " ".join(context.args).strip()
@@ -150,23 +156,23 @@ def votedump(update, context):
         resp+=j2["caption"]
     else:
         resp+="\n=================\nErr: Post not found"
-    update.message.reply_text(resp)
+    await update.message.reply_text(resp)
 
 
 
-def votelink(update, context):
+async def votelink(update, context):
     """ the user message."""   
     voteid = " ".join(context.args).strip()
-    update.message.reply_text( pago_real(voteid))
+    await update.message.reply_text( pago_real(voteid))
      
 
-def fee(update, context):
+async def fee(update, context):
     txt=""
     #https://api.yup.io/bridge/status    tru if bridge active
     r = requests.get("https://api.yup.io/bridge/fee-yup")
-    update.message.reply_text(r.text)
+    await update.message.reply_text(r.text)
 
-def pago_real(voteid):
+async def pago_real(voteid):
     txt=""
     r = requests.post('https://eos.greymass.com/v1/chain/get_table_rows', json=  {"json":"true",  "code":"yupyupyupyup",  "scope":"yupyupyupyup",  "table":"votev2",  "table_key":"",  "lower_bound":voteid,  "upper_bound":"null",  "index_position":1,  "key_type":"",  "limit":"1",  "reverse":"false",  "show_payer":"false"})
     j= r.json()
@@ -184,7 +190,7 @@ def pago_real(voteid):
     else:
         return "Errore: Post not found"
 
-def top_payment(update, context):
+async def top_payment(update, context):
     text=""
     if(len(context.args)<1):
        update.message.reply_text("usage: /top_payment eos_user \n"   
@@ -226,11 +232,11 @@ def top_payment(update, context):
     j2=r2.json()
     voteid= j2["execution_trace"]["action_traces"][0]["act"]["data"]["voteid"]
     text+="POSTURL: "
-    text+=pago_real(voteid) 
-    update.message.reply_text(text)
+    text+=await pago_real(voteid) 
+    await update.message.reply_text(text)
 
-def yup(update, context):
-    update.message.reply_text(
+async def yup(update, context):
+    await update.message.reply_text(
         random.choice(
             ["Give Me Yups Human DArme YUPSSSSSsssss!!!!!!",
             "YAP YAP YAP YAP YAP YAP",
@@ -256,7 +262,7 @@ def yup(update, context):
 """
         ]))
       
-def coffe(update, context):
+async def coffe(update, context):
     html="""
 <b>Buy Me a Coffe:</b>    
 
@@ -270,9 +276,9 @@ def coffe(update, context):
 stating by just  0.1 YUP 
 """
 
-    update.message.reply_html(html)
+    await update.message.reply_html(html)
 
-def power(update, context):
+async def power(update, context):
  
     """ the user message."""   
     user = " ".join(context.args).strip()    
@@ -283,20 +289,16 @@ def power(update, context):
     j["lastReset_VNZ"]=datetime.datetime.fromtimestamp( int(j["lastReset"])/1000,tz=tz.gettz('America/Caracas')).isoformat()[:16]
     #time.strftime("%x %X", time.localtime(j["lastReset"]/1000 ))
     
-    update.message.reply_text(json.dumps(j, indent=4, sort_keys=True) )
+    await update.message.reply_text(json.dumps(j, indent=4, sort_keys=True) )
 
-def priceold(update, context):
      
-    """ the user message."""   
-    r = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum,yup&vs_currencies=usd,eth")
-    j=r.json()
-    #time.strftime("%x %X", time.localtime(j["lastReset"]/1000 ))
-    txt="1 YUP = "+ str(j["yup"]["usd"] )+ " USD\n"
-    txt+=" \" \"  = "+ str(j["yup"]["eth"] )+ " ETH\n\n"
-    txt+="1 ETH  = "+ str(j["ethereum"]["usd"] )+ " USD\n"
-    update.message.reply_text(txt)
+    
 
-def price(update, context):
+def telegrammmm_error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    
+async def price(update, context):
      
     """ the user message."""   
     r = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum,yup&vs_currencies=usd,eth")
@@ -311,65 +313,76 @@ def price(update, context):
 
     yup yeah yap!
     """.format(str(j["yup"]["usd"] ),str(j["yup"]["eth"]),str(j["ethereum"]["usd"] ) )
-    update.message.reply_html(html)
+    await update.message.reply_html(html)
      
-    
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", start))
-    dp.add_handler(CommandHandler("postidof", postidof))
-    dp.add_handler(CommandHandler("votelink", votelink))    
-    dp.add_handler(CommandHandler("yup", yup))    
-    #dp.add_handler(CommandHandler("votedump", votedump))    
-    dp.add_handler(CommandHandler("power", power))    
-    dp.add_handler(CommandHandler("price", price))
-    dp.add_handler(CommandHandler("top_payment", top_payment))       
-    dp.add_handler(CommandHandler("tp", top_payment))  
-
-    dp.add_handler(CommandHandler("votesof", votesof))  
-    dp.add_handler(CommandHandler("votosde", votesof))  
-    dp.add_handler(CommandHandler("coffe", coffe))  
-    dp.add_handler(CommandHandler("cafe", coffe))      
-    dp.add_handler(CommandHandler("donar", coffe))  
-    dp.add_handler(CommandHandler("fee", fee))  
-    #dp.add_handler(CommandHandler("pricehtml", pricehtml))      
 
 
-    #Legacy
-    dp.add_handler(CommandHandler("pago", votelink))
-    dp.add_handler(CommandHandler("getpayment", top_payment))   
-    dp.add_handler(CommandHandler("link", postidof))    
-    # on noncommand i.e message - echo the message on Telegram
-    #dp.add_handler(MessageHandler(Filters.text, echo))
-    
-    # log all errors
-    dp.add_error_handler(error)
 
-    # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://yupyupyupyup.herokuapp.com/' + TOKEN)
+client = discord.Client()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+def iscmdfulltext(txt,cmd):    
+    return (len(txt.splitlines())==1) and "$"+cmd in [txt,txt.split()[0]]
 
-if __name__ == '__main__':
-    main()
+def iscmd(txt,cmd):    
+    return "$"+cmd == txt
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    #Bring Commands implemented on telegram    
+    txt=message.content
+    if len(txt.splitlines())>1:        
+        return
+    cmd,*args=txt.split()
+
+    update=TeleWrapper(message)
+    context=TeleContext(args)
+
+    if iscmd(cmd,"start"):
+        await start(update,context)
+    if iscmd(cmd,"help"):
+        await help(update,context)
+    if iscmd(cmd,"postidof"):
+        await postidof(update,context)
+    if iscmd(cmd,"votelink"):
+        await votelink(update,context)
+    if iscmd(cmd,"yup"):
+        await yup(update,context)
+    if iscmd(cmd,"votedump"):
+        await votedump(update,context)
+    if iscmd(cmd,"power"):
+        await power(update,context)
+    if iscmd(cmd,"price"):
+        await price(update,context)
+    if iscmd(cmd,"top_payment"):
+        await top_payment(update,context)
+    if iscmd(cmd,"tp"):
+        await top_payment(update,context)
+    if iscmd(cmd,"votesof"):
+        await votesof(update,context)
+    if iscmd(cmd,"votosde"):
+        await votesof(update,context)
+    if iscmd(cmd,"coffe"):
+        await coffe(update,context)
+    if iscmd(cmd,"cafe"):
+        await coffe(update,context)
+    if iscmd(cmd,"donar"):
+        await coffe(update,context)
+    if iscmd(cmd,"fee"):
+        await fee(update,context)
+              
+
+
+client.run(os.getenv('YUPDISCORDTOKEN'))
+
